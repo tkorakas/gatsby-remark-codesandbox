f42f11659672d0ac8488ccc0fa8d89aa383f01f5
Thanos Korakas <thanoskorakas@Thanoss-MacBook-Pro.local>
2021-12-29 21:28:26 +0200

    removed packages

--- packages/example/content/page1.md
+++
@@ -1,5 +0,0 @@
-# Hello world
-
-[embedded ode](codesandbox-link://example)
-
-[embedded ode](codesandbox-embed://example)
--- packages/example/src/templates/template.js
+++
@@ -1,5 +0,0 @@
-import React from 'react';
-
-export default function Template ({pageContext: {data}}) {
-    return <div dangerouslySetInnerHTML={{__html: data.html}} />
-}
